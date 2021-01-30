778697dba96f79def01186b2c8167e300f7b51c6
Jordan Masone <jordanmasone1@gmail.com>
2021-01-30 10:03:02 -0500

    Setup controller

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const mysql = require("mysql");
 const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
-// const routes = require("./controllers/burger_controllers");
+const routes = require("./controllers/burger_controllers");
 
 // Setup express connection
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 // Setup handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
