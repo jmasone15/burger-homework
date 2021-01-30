0dc515db29a176f74bf9117e363f85a93095f8f8
Jordan Masone <jordanmasone1@gmail.com>
2021-01-30 09:56:27 -0500

    Setup config files.

---
+++ config/orm.js
@@ -0,0 +1,8 @@
+const connection = require("./connection");
+
+const selectAll = () => {}
+const insertOne = () => {}
+const updateOne = () => {}
+const orm = {}
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -4,26 +4,13 @@ const mysql = require("mysql");
 const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const routes = require("./controllers/burger_controllers");
-
-// Setup sql database connection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-});
-connection.connect( (err) => {
-    if (err) throw err;
-    console.log(`Connected at id: ${connection.threadId}`);
-});
+// const routes = require("./controllers/burger_controllers");
 
 // Setup express connection
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 // Setup handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
