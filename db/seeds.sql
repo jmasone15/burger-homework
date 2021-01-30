f0c8893cc4fa5d1f0ebfd77172c805270c238071
Jordan Masone <jordanmasone1@gmail.com>
2021-01-30 09:50:27 -0500

    Setup burgers db.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Baconator", false);
